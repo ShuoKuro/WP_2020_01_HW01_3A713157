1e375f8f8b0198f8f5f626e73df4c75130e5d982
shuoya <timehet556947@gmail.com>
2020-11-07 20:42:41 +0800

    加入md5

--- WP_2020_HW01/Form1.Designer.cs
+++ WP_2020_HW01/Form1.Designer.cs
@@ -28,13 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.labShow1 = new System.Windows.Forms.Label();
+            this.labShow2 = new System.Windows.Forms.Label();
+            this.labShow3 = new System.Windows.Forms.Label();
+            this.labShow4 = new System.Windows.Forms.Label();
+            this.labShow5 = new System.Windows.Forms.Label();
+            this.labShow6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("新細明體", 20F);
+            this.tbInput.Location = new System.Drawing.Point(12, 30);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(288, 39);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("新細明體", 13F);
+            this.btnGo.Location = new System.Drawing.Point(343, 30);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(77, 39);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "确认";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // labShow1
+            // 
+            this.labShow1.AutoSize = true;
+            this.labShow1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow1.Location = new System.Drawing.Point(347, 80);
+            this.labShow1.Name = "labShow1";
+            this.labShow1.Size = new System.Drawing.Size(53, 20);
+            this.labShow1.TabIndex = 2;
+            this.labShow1.Text = "label1";
+            // 
+            // labShow2
+            // 
+            this.labShow2.AutoSize = true;
+            this.labShow2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow2.Location = new System.Drawing.Point(347, 130);
+            this.labShow2.Name = "labShow2";
+            this.labShow2.Size = new System.Drawing.Size(53, 20);
+            this.labShow2.TabIndex = 3;
+            this.labShow2.Text = "label1";
+            // 
+            // labShow3
+            // 
+            this.labShow3.AutoSize = true;
+            this.labShow3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow3.Location = new System.Drawing.Point(347, 180);
+            this.labShow3.Name = "labShow3";
+            this.labShow3.Size = new System.Drawing.Size(53, 20);
+            this.labShow3.TabIndex = 4;
+            this.labShow3.Text = "label1";
+            // 
+            // labShow4
+            // 
+            this.labShow4.AutoSize = true;
+            this.labShow4.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow4.Location = new System.Drawing.Point(347, 230);
+            this.labShow4.Name = "labShow4";
+            this.labShow4.Size = new System.Drawing.Size(53, 20);
+            this.labShow4.TabIndex = 5;
+            this.labShow4.Text = "label1";
+            // 
+            // labShow5
+            // 
+            this.labShow5.AutoSize = true;
+            this.labShow5.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow5.Location = new System.Drawing.Point(347, 280);
+            this.labShow5.Name = "labShow5";
+            this.labShow5.Size = new System.Drawing.Size(53, 20);
+            this.labShow5.TabIndex = 6;
+            this.labShow5.Text = "label1";
+            // 
+            // labShow6
+            // 
+            this.labShow6.AutoSize = true;
+            this.labShow6.Font = new System.Drawing.Font("新細明體", 15F);
+            this.labShow6.Location = new System.Drawing.Point(347, 330);
+            this.labShow6.Name = "labShow6";
+            this.labShow6.Size = new System.Drawing.Size(53, 20);
+            this.labShow6.TabIndex = 7;
+            this.labShow6.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label1.Location = new System.Drawing.Point(403, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label2.Location = new System.Drawing.Point(403, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label3.Location = new System.Drawing.Point(403, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "%";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label4.Location = new System.Drawing.Point(403, 230);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "%";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label5.Location = new System.Drawing.Point(403, 280);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 15F);
+            this.label6.Location = new System.Drawing.Point(403, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 137);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 12);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "label7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(429, 357);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labShow6);
+            this.Controls.Add(this.labShow5);
+            this.Controls.Add(this.labShow4);
+            this.Controls.Add(this.labShow3);
+            this.Controls.Add(this.labShow2);
+            this.Controls.Add(this.labShow1);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label labShow1;
+        private System.Windows.Forms.Label labShow2;
+        private System.Windows.Forms.Label labShow3;
+        private System.Windows.Forms.Label labShow4;
+        private System.Windows.Forms.Label labShow5;
+        private System.Windows.Forms.Label labShow6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- WP_2020_HW01/Form1.cs
+++ WP_2020_HW01/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +17,21 @@ namespace WP_2020_HW01
         {
             InitializeComponent();
         }
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MD5 md5 = MD5.Create();//建立一個MD5
+                byte[] source = Encoding.Default.GetBytes(tbInput.Text);//將字串轉為Byte[]
+                byte[] crypto = md5.ComputeHash(source);//進行MD5加密
+                string result = Convert.ToBase64String(crypto);//把加密後的字串從Byte[]轉為字串
+                label7.Text=("MD5加密:  " + result);//輸出結果
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"不要亂輸入", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
